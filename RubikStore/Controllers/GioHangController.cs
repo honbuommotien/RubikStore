c56eeb3c8eab06678721e948d9201740efb2ea04
Le Quang Huy <qu4nghuyle@gmail.com>
2022-06-14 15:32:31 +0700

    ...

---
+++ RubikStore/Controllers/GioHangController.cs
@@ -0,0 +1,119 @@
+﻿using RubikStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RubikStore.Controllers
+{
+    public class GiohangController : Controller
+    {
+        dbRubikStoreDataContext data = new dbRubikStoreDataContext();
+        public List<Giohang> LayGioHang()
+        {
+            List<Giohang> listGiohang = Session["GioHang"] as List<Giohang>;
+            if (listGiohang == null)
+            {
+                listGiohang = new List<Giohang>();
+                Session["GioHang"] = listGiohang;
+            }
+            return listGiohang;
+        }
+        public ActionResult ThemGioHang(int id, string strURL)
+        {
+            List<Giohang> listGiohang = LayGioHang();
+            Giohang sanpham = listGiohang.Find(n => n.id == id);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(id);
+                listGiohang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+        private int TongSoluong()
+        {
+            int tong = 0;
+            List<Giohang> listgiohang = Session["GioHang"] as List<Giohang>;
+            if (listgiohang != null)
+            {
+                tong = listgiohang.Sum(n => n.iSoluong);
+            }
+            return tong;
+        }
+
+        private int TongSoLuongSanPham()
+        {
+            int tong = 0;
+            List<Giohang> listgiohang = Session["GioHang"] as List<Giohang>;
+            if (listgiohang != null)
+            {
+                tong = listgiohang.Count;
+            }
+            return tong;
+        }
+
+        private double TongTien()
+        {
+            double tongtien = 0;
+            List<Giohang> listGioHnag = Session["GioHang"] as List<Giohang>;
+            if (listGioHnag != null)
+            {
+                tongtien = listGioHnag.Sum(n => n.dThanhtien);
+            }
+            return tongtien;
+        }
+
+        public ActionResult GioHang()
+        {
+            List<Giohang> listGiohang = LayGioHang();
+            ViewBag.Tongsoluong = TongSoluong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoLuongSanPham();
+            return View(listGiohang);
+        }
+
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoluong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoLuongSanPham();
+            return PartialView();
+        }
+
+        public ActionResult XoaGioHang(int id)
+        {
+            List<Giohang> listGiohang = LayGioHang();
+            Giohang sanpham = listGiohang.SingleOrDefault(n => n.id == id);
+            if (sanpham != null)
+            {
+                listGiohang.RemoveAll(n => n.id == id);
+                return RedirectToAction("GioHang");
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult CapNhatGioHang(int id, FormCollection collection)
+        {
+            List<Giohang> listGiohang = LayGioHang();
+            Giohang sanpham = listGiohang.SingleOrDefault(n => n.id == id);
+            if (sanpham != null)
+            {
+                sanpham.iSoluong = int.Parse(collection["txtSoluong"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult XoaTatCaGioHang()
+        {
+            List<Giohang> listGiohang = LayGioHang();
+            listGiohang.Clear();
+            return RedirectToAction("GioHang");
+        }
+    }
+}
---
+++ RubikStore/Models/Giohang.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RubikStore.Models
+{
+    public class Giohang
+    {
+        dbRubikStoreDataContext data = new dbRubikStoreDataContext();
+        public int id { get; set; }
+        [Display(Name = "Tên")]
+        public string ten { get; set; }
+        [Display(Name = "Ảnh bìa")]
+        public string hinh { get; set; }
+        [Display(Name = "Giá bán")]
+        public Double gia { get; set; }
+        [Display(Name = "Số lượng")]
+        public int iSoluong { get; set; }
+        [Display(Name = "Thành tiền")]
+        public Double dThanhtien
+        {
+            get { return iSoluong * gia; }
+        }
+        public Giohang(int id)
+        {
+            this.id = id;
+            Rubik rubik = data.Rubiks.Single(n => n.id == id);
+            ten = rubik.ten;
+            hinh = rubik.hinh;
+            gia = double.Parse(rubik.gia.ToString());
+            iSoluong = 1;
+        }
+    }
+}
