24b21494524fa29ee343e8b9494dd6db7ad8bce5
Le Quang Huy <qu4nghuyle@gmail.com>
2022-06-14 13:27:14 +0700

    Add project files.

---
+++ RubikStore/Controllers/RubikController.cs
@@ -0,0 +1,19 @@
+﻿using RubikStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RubikStore.Controllers
+{
+    public class RubikController : Controller
+    {
+        dbRubikStoreDataContext data = new dbRubikStoreDataContext();
+        public ActionResult Index()
+        {
+            var all_rubik = from s in data.Rubiks select s;
+            return View(all_rubik);
+        }
+    }
+}
